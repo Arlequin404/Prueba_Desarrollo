2a2c061e1e81ff55d8ed8a66038a3cb177fd8e6b
alexo <alexortiz-31@hotmail.com>
2024-02-15 20:36:20 -0500

    version final

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
-from PyPDF2 import PdfReader
 import streamlit as st
-from langchain.document_loaders import rss
+from bs4 import BeautifulSoup
+import feedparser
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain import FAISS
@@ -10,69 +10,54 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 import langchain
-import pandas as pd
-from langchain_community.document_loaders import UnstructuredHTMLLoader
 
 langchain.verbose = False
-
 load_dotenv()
 
+def process_html(html_file):
+    html_text = html_file.read()
+    soup = BeautifulSoup(html_text, "html.parser")
+    text = soup.get_text()
+    return text
 
-def process_rss(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
-
-    chunks = text_splitter.split_text(text)
-
-    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
-
-    knowledge_base = FAISS.from_texts(chunks, embeddings)
-
-    return knowledge_base
-
-
-def process_html(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-
-    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
-
-    knowledge_base = FAISS.from_texts(chunks, embeddings)
-
-    return knowledge_base
-
+def process_rss(rss_file):
+    rss_text = rss_file.read()
+    feed = feedparser.parse(rss_text)
+    entries_text = [entry.title + entry.description for entry in feed.entries]
+    text = ' '.join(entries_text)
+    return text
 
 def main():
-    st.title("Preguntas a un archivo")
+    st.title("Preguntas a fuentes de texto")
 
-    file_type = st.radio("Selecciona el tipo de archivo:", ("rss", "html"))
+    html_file = st.file_uploader("Sube tu archivo HTML", type="html", key="html_file")
+    rss_file = st.file_uploader("Sube tu archivo RSS", type="rss", key="rss_file")
 
-    file = st.file_uploader("Sube tu archivo", type=["rss", "html"])
+    combined_text = ""
 
-    if file is not None:
-        if file_type == "rss":
-            rss_reader = rss.Reader(file)
-            text = ""
-            for page in rss_reader.pages:
-                text += page.extract_text()
-            knowledge_base = process_rss()
-        elif file_type == "html":
+    if html_file is not None:
+        html_text = process_html(html_file)
+        combined_text += html_text + " "
 
-            uploader = UnstructuredHTMLLoader()
-            data =uploader.load()
-            data
+    if rss_file is not None:
+        rss_text = process_rss(rss_file)
+        combined_text += rss_text
 
+    if combined_text:
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
 
-        query = st.text_input('Escribe tu pregunta para el archivo...')
+        chunks = text_splitter.split_text(combined_text)
+
+        embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+        query = st.text_input('Escribe tu pregunta...')
         cancel_button = st.button('Cancelar')
 
         if cancel_button:
@@ -88,7 +73,6 @@ def main():
                 response = chain.invoke(input={"question": query, "input_documents": docs})
                 print(cost)
                 st.write(response["output_text"])
-
 
 if __name__ == "__main__":
     main()
